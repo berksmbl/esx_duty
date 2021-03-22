85f012d62474bdcd3e3855de11c998f81e54af80
Berk Sümbül <berksmbl@gmail.com>
2021-03-22 10:32:08 +0300

    Added Sheriff

--- config.lua
+++ config.lua
@@ -10,11 +10,20 @@ Config.Zones = {
     Type  = 27,
     JobRequired = "police"
   },
+  
   Ambulance = {
     Pos = { x = 310.72, y = -595.93, z = 42.29 },
     Size = { x = 2.5, y = 2.5, z = 1.5 },
     Color = { r = 0, g = 255, b = 0 },
     Type = 27,
     JobRequired = "ambulance"
+  },  
+  
+  Sheriff = {
+    Pos = { x = -450.1, y = 6012.78, z = 30.8 },
+    Size = { x = 1.5, y = 1.5, z = 1.5 },
+    Color = { r = 0, g = 255, b = 0 },
+    Type = 27,
+    JobRequired = "sheriff"
   },
 }
